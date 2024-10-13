56afd349cd6db0719316ae8eeae2b476b31f23d8
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-10-13 18:51:03 +0530

    Added user and quiz model

---
+++ models/quiz.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  questionText: {
+    type: String,
+    required: true,
+  },
+  options: [{ type: String, required: true }],
+  correctAnswer: {
+    type: Number,
+    required: true,
+  },
+});
+
+const quizSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  questions: [questionSchema],
+});
+
+module.exports = mongoose.model("Quiz", quizSchema);
